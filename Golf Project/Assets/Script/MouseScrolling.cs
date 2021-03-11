4861d73c964946e9d3121bb6fb68046d3d8b04dd
Guillaume <g.roge@rubika-edu.com>
2021-03-11 09:55:16 +0100

    Add unity project

---
+++ Golf Project/Assets/Script/MouseScrolling.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MouseScrolling : MonoBehaviour
+{
+    public CinemachineFreeLook playerCam;
+    
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        Zoom();
+    }
+
+    void Zoom()
+    {
+        if (Input.mouseScrollDelta.y > 0 && playerCam.m_Orbits[2].m_Radius > 0.5f && playerCam.m_Orbits[0].m_Radius > 0.5f)
+        {
+            playerCam.m_Orbits[0].m_Radius -= 0.2f;
+            playerCam.m_Orbits[0].m_Height -= 0.15f;
+
+            playerCam.m_Orbits[1].m_Radius -= 0.2f;
+            playerCam.m_Orbits[2].m_Radius -= 0.2f;
+        }
+        if (Input.mouseScrollDelta.y < 0 && playerCam.m_Orbits[0].m_Radius < 7f)
+        {
+            playerCam.m_Orbits[0].m_Radius += 0.2f;
+            playerCam.m_Orbits[0].m_Height += 0.15f;
+
+            playerCam.m_Orbits[1].m_Radius += 0.2f;
+            playerCam.m_Orbits[2].m_Radius += 0.2f;
+        }
+    }
+}
---
+++ Golf Project/Assets/Script/PlayerShoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float force;
+    public Rigidbody playerRb;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.forward * force);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            playerRb.AddForce(Vector3.up * force / 3f);
+        }
+    }
+}
