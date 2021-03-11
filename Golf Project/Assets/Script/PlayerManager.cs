0e05196d19f24911cf5a314cc55baeccec03cd3e
Guillaume <g.roge@rubika-edu.com>
2021-03-11 20:37:37 +0100

    Player manager

---
+++ Golf Project/Assets/Script/PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager instance;
+
+    public bool canShoot;
+    public bool isMoving;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
